eaca1b26eeacef804edeeb76e9ff48efff7cc3b7
olzhas81 <41197999+olzhas81@users.noreply.github.com>
2018-07-13 20:17:18 +0600

    Beta 0.1
    Загрузка файлов.

---
+++ script.js
@@ -0,0 +1,62 @@
+  //alert('Vneshnyy stranica');
+  //coments
+  /* coments
+  coments
+  coments */
+  var massange='Privet';
+  //alert(massange);
+  massange='Poka';
+  //alert(massange);
+
+  //var result=prompt('God', '');
+
+ //var line=confirm('question');
+  //alert(line);
+
+/*var year=prompt('год ЧМ', '');
+  if(year == 2018) {
+    alert('Verno');
+  } else if (year < 2018) {
+    alert('pozdno');
+  } else {
+    alert('pravilno');
+  }*/
+
+function functionName() {
+  var year=prompt('год ЧМ', '');
+  if(year == 2018) {
+    alert('Verno');
+  }
+    while (year < 2018 || year > 2018) {
+    year=prompt('год ЧМ', '');
+  }
+}
+
+functionName();
+functionName();
+
+
+
+
+
+  /*else {
+    alert('Verno');
+  }*/
+
+/*var x=10;
+  do {
+    alert(x);
+    x++;
+  } while (x<3);*/
+
+
+  /*for(var x=0; x<10; x++){
+    alert(x);
+  }*/
+
+/*  function lion() {
+    alert("функция");
+  }
+
+  lion();
+  lion();*/
